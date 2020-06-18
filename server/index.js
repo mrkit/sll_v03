ba73fbbbfecc8adfac15135c0a15c0e90625749b
panda <neptmanhall@gmail.com>
2020-06-18 11:35:33 -0400

    made more readable

--- server/index.js
+++ server/index.js
@@ -1,34 +1,30 @@
-const e = require('express'),
-      a = e(),
-      r = require('path').resolve,
-      m = require('morgan'),
-      bp = require('body-parser'),
+const express = require('express'),
+      app = express(),
+      resolve = require('path').resolve,
+      morgan = require('morgan'),
+      {json, urlencoded } = require('body-parser'),
       db = require('./db'),
       force = process.env.NODE_ENV === 'development' ? true : false;
 
-// console.log('Node env', process.env.NODE_ENV);
-
-a.use('/assets', e.static(r(__dirname, '..', 'assets')));
-a.use([
+app.use('/assets', express.static(resolve(__dirname, '..', 'assets')));
+app.use([
   e.static(r(__dirname, '..', 'dist')),
-  m('dev'),
-  bp.json(),
-  bp.urlencoded({ extended: false })
+  morgan('dev'),
+  json(),
+  urlencoded({ extended: false })
 ]);
 
-a.use('/api', require('./api'));
-a.get('/*', (req, res) => res.sendFile(r(__dirname, '..', 'dist', 'index.html')));
+app.use('/api', require('./api'));
+app.get('/*', (req, res) => res.sendFile(resolve(__dirname, '..', 'dist', 'index.html')));
 
-a.use((err, req, res, next) => {
+app.use((err, req, res, next) => {
   if(err) {
-    console.log(`Catch All Error message: ${err.message}`);
+    console.error(`Catch All Error message: ${err.message}`);
     res.send(err.message);
   }
 });
 
-a.listen(3006, console.log('listening on 3006'));
-
-//db.conn.sync({ force })
-//.then(() => db.models.Users.create({ username: 'luisa', password: 'tokyo' }))  
-//.then(() => db.models.Posts.create({title: 'Test Title', date:'11/2/2018', author: 'john', article: 'Test post'}))
-//.then(() => a.listen(3006, console.log('listening on 3006')));
+db.conn.sync({ force })
+.then(() => db.models.Users.create({ username: 'luisa', password: 'tokyo' }))  
+.then(() => db.models.Posts.create({title: 'Test Title', date:'11/2/2018', author: 'john', article: 'Test post'}))
+.then(() => app.listen(3006, console.log('listening on 3006')));
